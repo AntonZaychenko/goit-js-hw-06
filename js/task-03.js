830418a9777de118537042128d5eb3572c314aff
AntonZaychenko <antonyzaychenko30012001@gmail.com>
2022-07-18 13:14:16 +0300

    change

--- js/task-03.js
+++ js/task-03.js
@@ -2,17 +2,17 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
-    width: 200,
+   
   },
   {
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    width: 200,
+    
   },
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-    width: 200,
+   
   },
 ];
 
@@ -26,7 +26,7 @@ const images = [
   const galleryImgEl = document.createElement("img");
   galleryImgEl.src = image.url;
   galleryImgEl.alt = image.alt;
-  galleryImgEl.width = image.width;
+ 
 
   galleryItemEl.appendChild(galleryImgEl);
 
